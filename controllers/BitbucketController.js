61a250d6bf1950080c2fd06373ccc82567d73f93
nesrine-snoussi <nesrine.snoussi@esprit.tn>
2024-05-23 18:27:52 +0100

    fetch projects from bitbucket

--- controllers/BitbucketController.js
+++ controllers/BitbucketController.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 // Function to get the repository URL from Bitbucket
 async function connect_bitbucket(req, res) {
-    const accessToken = req.body.access_token;
+    const accessToken = req.body.accessToken;
     const workspace = req.body.workspace;
 
     if (!accessToken || !workspace) {
@@ -12,7 +12,9 @@ async function connect_bitbucket(req, res) {
     try {
         const repositories = await fetchRepositoriesFromBitbucket(accessToken, workspace);
         // Optionally, store the repositories in a cookie or session here
-        res.send({ success: true, repositories });
+        res.cookie('repositories', JSON.stringify(repositories), { maxAge: 86400000, httpOnly: true });
+        res.send({ repositories });
+        console.log(JSON.stringify(repositories));
     } catch (error) {
         console.error(error);
         res.status(500).send({ error: 'Failed to fetch repositories.' });
@@ -28,7 +30,7 @@ async function fetchRepositoriesFromBitbucket(accessToken, workspace) {
                 Accept: 'application/json',
             },
         });
-        // Map over the repositories array to include additional details
+
         const enhancedRepositories = response.data.values.map(repo => ({
             name: repo.name,
             description: repo.description || repo.summary,
@@ -36,9 +38,12 @@ async function fetchRepositoriesFromBitbucket(accessToken, workspace) {
             lastUpdated: repo.updated_on,
             cloneUrl: repo.links.clone[0].href,
             language: repo.language,
-        }));
+            //slug: repo.slug,
 
-        return enhancedRepositories; // Corrected to return the repositories instead of sending them in the response
+        }));
+        return { enhancedRepositories };
+
+
     } catch (error) {
         throw new Error('Failed to fetch repositories from Bitbucket.');
     }
