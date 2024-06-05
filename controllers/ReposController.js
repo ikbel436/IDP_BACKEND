c9f925997c4cae25abed330770ab0084ed3860b9
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2024-06-06 00:42:47 +0200

    Repo changes made

--- controllers/ReposController.js
+++ controllers/ReposController.js
@@ -1,70 +1,65 @@
-
-const Repository = require('../models/Repositories');
+const Repository = require("../models/Repositories");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const secretOrKey = config.get("secretOrKey");
 
-
 exports.createRepository = async (req, res) => {
-    try {
-        const repositories = req.body; 
-        const savedRepositories = await Repositories.insertMany(repositories);
-        res.json(savedRepositories);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error saving repositories.' });
-      }
-  };
-  
-  exports.getAllRepositories = async (req, res) => {
-    try {
-        const repositories = await Repository.find({});
-        res.json(repositories);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error retrieving repositories.' });
-      }
-  };
+  try {
+    const repositories = req.body;
+    const savedRepositories = await Repositories.insertMany(repositories);
+    res.json(savedRepositories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error saving repositories." });
+  }
+};
 
+exports.getAllRepositories = async (req, res) => {
+  try {
+    const repositories = await Repository.find({});
+    res.json(repositories);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error retrieving repositories." });
+  }
+};
 
-  exports.updateRepository = async (req, res) => {
-    try {
-      const { id } = req.params;
-      const updatedData = req.body;
-  
-      const result = await Repository.findByIdAndUpdate(id, updatedData, { new: true });
-  
-      if (!result) {
-        return res.status(404).json({ message: 'Repository not found' });
-      }
-  
-      res.json(result);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error updating repository.' });
+exports.updateRepository = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedData = req.body;
+
+    const result = await Repository.findByIdAndUpdate(id, updatedData, {
+      new: true,
+    });
+
+    if (!result) {
+      return res.status(404).json({ message: "Repository not found" });
     }
-  };
 
-
-
+    res.json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating repository." });
+  }
+};
 
 exports.getRepoById = async (req, res) => {
   try {
-    const { id } = req.params; 
+    const { id } = req.params;
 
     const repository = await Repository.findById(id);
 
     if (!repository) {
-      return res.status(404).json({ message: 'Repository not found' });
+      return res.status(404).json({ message: "Repository not found" });
     }
 
     res.json(repository);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Error retrieving repository.' });
+    res.status(500).json({ message: "Error retrieving repository." });
   }
 };
-
 
 // create Repo and assign it to a user
 exports.AddRepotoUser = async (req, res) => {
@@ -73,10 +68,12 @@ exports.AddRepotoUser = async (req, res) => {
     description,
     cloneUrl,
     language,
+    createdAt,
+    lastUpdated,
     DBType,
-    DockerImage , 
-    Status ,
-    SonarQube 
+    DockerImage,
+    Status,
+    SonarQube,
   } = req.body;
 
   const authHeader = req.headers.authorization;
@@ -96,10 +93,12 @@ exports.AddRepotoUser = async (req, res) => {
       description,
       cloneUrl,
       language,
+      createdAt,
+      lastUpdated,
       DBType,
-      DockerImage , 
-      Status ,
-      SonarQube 
+      DockerImage,
+      Status,
+      SonarQube,
     });
 
     await newRepo.save();
@@ -152,7 +151,7 @@ exports.retreive = [
 
       const projects = user.myRepo;
 
-      res.status(200).json( {projects} );
+      res.status(200).json({ projects });
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
