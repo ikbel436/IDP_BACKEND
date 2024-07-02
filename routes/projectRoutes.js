017c1a2776fc52e47e7a7936d928c8782324651a
Zaghouani Yosri <yosri.zaghouani@esprit.tn>
2024-07-02 13:19:21 +0200

    Kuberenetes Secret Added

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -19,8 +19,5 @@ router.put('/project/:id', updateProject);
 router.delete('/projects/:id', deleteProject);
 router.get("/get",retreive);
 router.get("/get/:id",retreivebyId);
-router.post('/generate-configmap', generateConfigMapFile);
-router.post('/generate-database-deployment',generateDataBaseFile);
-router.post('/generate-deployment',generateDeploymentFile);
-router.post('/apply-generated-k8s-files', applyGeneratedK8sFiles ); 
+
 module.exports = router;
